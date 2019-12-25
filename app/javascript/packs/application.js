87e437c58c54976bff979a71c79f78ebe18b5a83
wijinlau <wi6@me.com>
2019-12-25 12:15:10 +0800

    finish #5

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,9 +12,9 @@ import 'styles/full_site.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+// footer置底
+document.addEventListener("turbolinks:load", ()=>{
+    if (document.querySelector('main').offsetHeight < screen.height) {
+        document.querySelector("footer").classList.add('bottem_footer');
+    }
+}, false);
