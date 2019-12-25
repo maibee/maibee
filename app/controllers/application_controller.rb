dfd06fac44f06ea83ebef44606676f4262bf3082
wijinlau <wi6@me.com>
2019-12-25 22:52:45 +0800

    notification WIP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :records #for notifications on header
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
+  def records
+    @records = Record.where(user: current_user).last(10)
   end
 end
