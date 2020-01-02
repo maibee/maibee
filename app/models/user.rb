b477b5366480b381a4fa2a1458bf53356a1e5444
wijinlau <wi6@me.com>
2020-01-02 16:25:13 +0800

    give every new user a ntd wallet

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,34 +1,25 @@
 class User < ApplicationRecord
   include AASM
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  #驗證
-  # validates :last_name, presence: true
-  # validates :first_name, presence: true
-
-  # before_save do
-  #   if self.wallet_id == nil
-  #     wallet = Wallet.create
-  #     self.wallet_id = wallet.id
-  #   end
-  # end
   has_many :wallets
   has_many :records
-
   has_many :transfers
   has_many :limit_orders
   has_many :transaction_records
+
+  after_create do
+    self.wallets.create(currency_id: Currency.find_by(name:"NTD").id, amount: 0)
+  end
 
   def make_limit_order(order_content)
     my_wallet = find_wallet(order_content[:currency_id], self.id)
 
     if ((my_wallet.amount >= order_content[:amount]) &&
        self.limit_orders.create(currency_id: order_content[:currency_id],
-                               amount: order_content[:amount],
-                               status: "pending",
-                               sell_price: order_content[:sell_price]))
+                                amount:      order_content[:amount],
+                                status:      "pending",
+                                sell_price:  order_content[:sell_price]))
        my_wallet.amount -= order_content[:amount]
        my_wallet.save
     end
