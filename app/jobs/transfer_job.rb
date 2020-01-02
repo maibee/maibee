cca0bb326e9b7e6fdfff77e226f1c866e88f28d9
wijinlau <wi6@me.com>
2020-01-02 11:51:09 +0800

    link api keys

--- app/jobs/transfer_job.rb
+++ app/jobs/transfer_job.rb
@@ -2,8 +2,7 @@ class TransferJob < ApplicationJob
   queue_as :default
 
   def perform(transfer)
-    coin = "DOGECOIN" 
-    BlockIo.set_options :api_key=> ENV[coin], :pin => ENV["PIN"], :version => 2
+    BlockIo.set_options :api_key=> ENV[transfer.currency.name], :pin => ENV["pin"], :version => 2
     BlockIo.withdraw :amounts => transfer.amounts.to_s, :to_addresses => transfer.target
     transfer.txid = (BlockIo.get_transactions :type => 'sent')["data"]["txs"][0]["txid"]
     transfer.save
