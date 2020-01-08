6b7b1ad9d67088cc998c88a9145775827c534e56
wijinlau <wi6@me.com>
2020-01-08 18:36:11 +0800

    show accets rank

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,16 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :records #for notifications on header
 
+  def demo_rank
+    Wallet.all.map{|w| [w.user_id, (w.amount * get_rate(w.currency_id))]}
+      .reduce([[]]){|accu, w| accu[0] == w[0] ? }
+  end
+
   private
+
+  def get_rate(currency_id)
+    Currency.find_by(id: currency_id).last_rate
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
