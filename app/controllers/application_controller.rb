e4d8822f5b6bbed4bf226443a5d81c7415d5c637
wijinlau <wi6@me.com>
2020-01-08 17:33:18 +0800

    make honey point the default trading unit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,23 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :records #for notifications on header
+  before_action :records
 
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
+
   def records
     @records = current_user.records.last(10).reverse if current_user
   end
+
   def show_menu
     @show_menu = true
+  end
+
+  def check_user_state
+    authenticate_user!
+    redirect_to confirmation_letters_path if current_user == 'uncertified'
   end
 end
--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -1,21 +1,18 @@
 class ExchangesController < ApplicationController
+  before_action :check_user_state
   def index
-    
     @currencies = Currency.tradable
   end
+
   def show
-    active?
     @currency = Currency.find_by(id: params[:id])
-    @order = Order.new
-    @currencies = Currency.tradable.map{|c| [c.name, c.id] }
+    @order = Order.new(currency_id: params[:id])
+    @currencies = Currency.tradable.map{ |c| [c.name, c.id] }
+    @amount = get_wallet ? get_wallet.amount : 0
   end
 
   private
-  def active?
-    
-    if current_user.status == false
-      redirect_to confirmation_letters_path
-    end
+  def get_wallet
+    current_user.wallets.where(currency_id: @currency.id)[0]
   end
-
 end
