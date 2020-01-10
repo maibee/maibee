dcb7d73a32d5068fd21e4c530ec97bd85dcd3fda
Gary <garyqwertyuiop@gmail.com>
2020-01-10 15:25:43 +0800

    check records 0 or not

--- app/javascript/zeroing/homepage.js
+++ app/javascript/zeroing/homepage.js
@@ -4,17 +4,22 @@ document.addEventListener("DOMContentLoaded", function(){
   console.log("ready");
   document.getElementById('notice_record').addEventListener("click", function(){
     
-    ax({
-      method: 'post',
-      url: '/records/zeroing',
-      data: {
-      }
-    }).then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-    document.getElementById('bell').innerText= 0
+    
+    if(this.getAttribute('name') === 'unread'){
+      ax({
+        method: 'post',
+        url: '/records/zeroing',
+        data: {
+        }
+      }).then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+      document.getElementById('bell').innerText= 0
+      this.setAttribute('name', "read")
+    }
   })
+  
 }); 
