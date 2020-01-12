bc2ca75655091a21d9ddc0e6c6c65deebec5c999
wijinlau <wi6@me.com>
2020-01-12 15:56:57 +0800

    price query

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -3,4 +3,26 @@ class Currency < ApplicationRecord
   extend FriendlyId
   friendly_id :codename, use: :slugged
   has_many :latest_prices
+
+
+  def latest
+    unless self.latest_prices.last
+      update_price
+    else
+      if (self.latest_prices.last.created_at + 60*60) < Time.now
+        update_price
+      end
+    end
+    self.latest_prices.last
+  end
+
+  def update_price
+    query = PriceQuery.new([
+      Currency.find_by(codename: 'BTC'),
+      Currency.find_by(codename: 'LTC'),
+      Currency.find_by(codename: 'DOGE')
+    ])
+    query.get_price
+  end
+
 end
