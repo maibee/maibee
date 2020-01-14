04d3b120fe51618888d9d7092a8035c881962211
wijinlau <wi6@me.com>
2020-01-14 22:21:16 +0800

    add extra port for spq

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -4,16 +4,13 @@ class Currency < ApplicationRecord
   friendly_id :codename, use: :slugged
   has_many :latest_prices
 
-  # def last_rate
-  #   if self.latest == nil
-  #     1
-  #   else
-  #   self.latest.price
-  #   end
-  # end
+  def last_rate
+    self.latest.price
+  end
 
   def latest
     return honey_point_price if self.codename == 'HP'
+    return spq_price if self.codename == 'SPQ'
     unless self.latest_prices.last
       update_price
     else
@@ -36,6 +33,13 @@ class Currency < ApplicationRecord
   def honey_point_price
     if self.latest_prices.last == nil
       self.latest_prices.create(price: 1)
+    end
+    self.latest_prices.last
+  end
+
+  def spq_price
+    if self.latest_prices.last == nil
+      self.latest_prices.create(price: 50)
     end
     self.latest_prices.last
   end
